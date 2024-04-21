1c22f0d4de27775f81db4e7996d2889cce35922b
Mohammad I-K Sakur <mdiftakharkabirsakur@gmail.com>
2024-04-22 01:04:04 +0600

    meta file

---
+++ Assets/meta file/aboutobject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class aboutobject : MonoBehaviour
+{
+     public string nextSceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.LoadScene(nextSceneName);
+    }
+
+    // Update is called once per frame
+  
+}
